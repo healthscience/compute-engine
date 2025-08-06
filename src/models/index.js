85616a1da240ef8f9d58273a5dd2e145edbf9664
aboynejames <aboynejames@gmail.com>
2025-08-06 10:19:49 +0100

    add absolute paths to import models for windows

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,36 @@
-export { default as AverageModel } from './statistics/average.js';
-export { default as SumModel } from './statistics/sum.js';
-export { default as LinearRegression } from './statistics/linear-regression.js';
-export { default as AutoRegression } from './statistics/auto-regression.js';
+import os from 'os'
+import path from 'path';
+
+let exports = {};
+
+if (os.platform() === 'win32') {
+  console.log('window path');
+  const avgPath = path.resolve('./statistics/average.js');
+  const sumPath = path.resolve('./statistics/sum.js');
+  const linregPath = path.resolve('./statistics/linear-regression.js');
+  const autoregPath = path.resolve('./statistics/auto-regression.js');
+
+  exports = {
+    AverageModel: (await import(avgPath)).default,
+    SumModel: (await import(sumPath)).default,
+    LinearRegression: (await import(linregPath)).default,
+    AutoRegression: (await import(autoregPath)).default
+  };
+} else {
+  console.log('non windows');
+  exports = {
+    AverageModel: (await import('./statistics/average.js')).default,
+    SumModel: (await import('./statistics/sum.js')).default,
+    LinearRegression: (await import('./statistics/linear-regression.js')).default,
+    AutoRegression: (await import('./statistics/auto-regression.js')).default
+  };
+}
+
+export default exports;
+
+/*
+  export { default as AverageModel } from './statistics/average.js';
+  export { default as SumModel } from './statistics/sum.js';
+  export { default as LinearRegression } from './statistics/linear-regression.js';
+  export { default as AutoRegression } from './statistics/auto-regression.js'
+*/
